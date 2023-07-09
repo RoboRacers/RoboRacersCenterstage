3ed7c95405d1c6dcbda38e0619520e5df02e468c
Vikram Kommera <outcastvirus24@icloud.com>
2023-07-09 09:28:16 -0700

    Update ParticleMap.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/gaeldrive/geometry/ParticleMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/gaeldrive/geometry/ParticleMap.java
@@ -1,12 +1,15 @@
 package org.firstinspires.ftc.teamcode.modules.gaeldrive.geometry;
 
+import android.os.Build;
+
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ParticleMap {
 
-    HashMap<Integer, Particle2d> Particles = new HashMap<>();;
-    HashMap<Integer, Double> ParticleWeights = new HashMap<>();;
+    HashMap<Integer, Particle2d> Particles = new HashMap<>();
+    HashMap<Integer, Double> ParticleWeights = new HashMap<>();
 
     //TODO: Get Highest Weighed Particle
     public void add(Particle2d particle) {
@@ -20,12 +23,12 @@ public class ParticleMap {
     }
 
     public void getBestParticle () {
-        Map.Entry<Integer, String> entryWithLargestKey = ParticleWeights
-                .entrySet()
-                .stream()
-                .sorted(Map.Entry.comparingByKey(Comparator.reverseOrder()))
-                .findFirst()
-                .get();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            Collections.max(ParticleWeights.entrySet(), Map.Entry.comparingByValue()).getKey();
+        }
+        
+
     }
 
 

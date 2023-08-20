ce4a0e21370303663a0ffdce8d50b838d881a514
superiorcave <113873020+superiorcave@users.noreply.github.com>
2023-08-20 15:35:40 -0700

    Update PID_Ball_Control.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/PID_Ball_Control.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/PID_Ball_Control.java
@@ -1,4 +1,24 @@
 package org.firstinspires.ftc.teamcode.teleop;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.teamcode.modules.drive.SampleMecanumDrive;
 
-public class PID_Ball_Control {
+@TeleOp(name = "PID_Ball_Control", group = "16481-Centerstage")
+public class PID_Ball_Control extends LinearOpMode{
+    //define sensors and servo here
+    //also varaibles
+    private DistanceSensor distance_sensor_range;
+    private Servo balance_servo;
+    @Override
+    public void runOpMode(){
+        //stuff goes here
+    }
+
+
 }

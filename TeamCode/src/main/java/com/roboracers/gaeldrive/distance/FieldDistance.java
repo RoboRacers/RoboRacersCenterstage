31d9bcc6630c804172204f580708e464a7f4e509
Vikram Kommera <outcastvirus24@icloud.com>
2023-08-25 07:44:51 -0700

    Update

--- TeamCode/src/main/java/com/roboracers/gaeldrive/distance/FieldDistance.java
+++ TeamCode/src/main/java/com/roboracers/gaeldrive/distance/FieldDistance.java
@@ -8,6 +8,8 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.roboracers.gaeldrive.utils.VectorUtils;
 
+import org.apache.commons.math3.linear.ArrayRealVector;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -56,7 +58,7 @@ public class FieldDistance {
 
         List<Double> distances = new ArrayList<>();
 
-        for(Vector2d[] line : FieldGeometry) {
+        for(Vector2d[] line : FieldGeometry) { //TODO: Point of improvement - currently checks every line, more time copmplexity
             double distance = getRayToLineSegmentIntersection(origin, line[0], line[1]);
             if (distance != -1 ) {
                 distances.add(distance);
@@ -77,6 +79,28 @@ public class FieldDistance {
      * @return Distance between ray and line
      */
     public static double getRayToLineSegmentIntersection(Pose2d pose, Vector2d point1, Vector2d point2) {
+
+        Vector2d rayOrigin = new Vector2d(pose.getX(), pose.getY());
+        Vector2d v1 = rayOrigin.minus(point1);
+        Vector2d v2 = point2.minus(point1);
+        Vector2d v3 = new Vector2d(-sin(pose.getHeading()), cos(pose.getHeading()));
+        double dot = v2.dot(v3);
+
+        if (abs(dot) < 0.000001) {
+            return -1;
+        }
+
+        double t1 = VectorUtils.CrossProduct2d(VectorUtils.Vector2dToVector(v2), VectorUtils.Vector2dToVector(v1))/dot;
+        double t2 = (v1.dot(v3)) / dot;
+
+        if (t1 >= 0.0 && (t2 >= 0.0 && t2 <= 1.0)) {
+            return t1;
+        }
+
+        return -1;
+    }
+
+    public static double getRayToLineSegmentIntersection2(Pose2d pose, Vector2d point1, Vector2d point2) {
 
         Vector2d rayOrigin = new Vector2d(pose.getX(), pose.getY());
         Vector2d v1 = rayOrigin.minus(point1);
--- TeamCode/src/main/java/com/roboracers/gaeldrive/tests/WeightingUnitTest.java
+++ TeamCode/src/main/java/com/roboracers/gaeldrive/tests/WeightingUnitTest.java
@@ -4,6 +4,7 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.roboracers.gaeldrive.filters.ParticleFilter2d;
 import com.roboracers.gaeldrive.sensors.SensorModel;
 import com.roboracers.gaeldrive.sensors.TestDistanceSensorModel;
+import com.roboracers.gaeldrive.utils.Updatable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,6 +29,11 @@ public class WeightingUnitTest {
         models.add(new TestDistanceSensorModel(51, pose2));
         models.add(new TestDistanceSensorModel(51, pose3));
 
+        for (Updatable model: models
+             ) {
+            model.update();
+        }
+
         System.out.println("Actual Sensor Reading: " + models.get(0).getActualReading() + ", Relative Sensor Location " + pose1);
         System.out.println("Actual Sensor Reading: " + models.get(1).getActualReading() + ", Relative Sensor Location: " + pose2);
         System.out.println("Actual Sensor Reading: " + models.get(2).getActualReading() + ", Relative Sensor Location: " + pose3);
@@ -37,7 +43,7 @@ public class WeightingUnitTest {
 
         System.out.println("Best Particle Pose: " + filter.getBestPose());
         System.out.println("Best Particle Weight: " + filter.getBestParticle().getWeight());
-        System.out.println("Time for function call: " + (loopTime-loop) + "ns");
+        System.out.println("Time for function call: " + (loopTime-loop)/1000000 + "ms");
 
         System.out.println("Unit Test Ended!");
         System.out.println("* * * * * * * * * * * *");

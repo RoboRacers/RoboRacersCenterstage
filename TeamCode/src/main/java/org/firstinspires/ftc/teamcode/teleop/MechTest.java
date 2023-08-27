1751641d137b49be6392121f542da1d71c225cb6
ishaan-ahuja <ishaan.ahuja.102@gmail.com>
2023-08-27 15:22:43 -0700

    mecanum move

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MechTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MechTest.java
@@ -30,13 +30,30 @@ public class MechTest extends LinearOpMode {
         LBMotor = hardwareMap.get(DcMotor.class,"Bl");
 
 
+//        public void mecMove(){
+//
+//            double vertical;
+//            double horizontal;
+//            double pivot;
+//            vertical=-gamepad1.left_stick_y;
+//            horizontal=gamepad1.left_stick_x;
+//            pivot=gamepad1.right_stick_x;
+//
+//
+//            RFMotor. setPower (pivot + (-vertical + horizontal));
+//            RBMotor. setPower (pivot + (-vertical - horizontal));
+//            LFMotor. setPower(pivot + (-vertical - horizontal));
+//            RFMotor. setPower (pivot + (-vertical + horizontal));
 
-        while (opModeInInit()) {
-        }
-
-        while (!isStopRequested()) {
-
-        }
+//
+//        }
+//
+//        while (opModeInInit()) {
+//        }
+//
+//        while (!isStopRequested()) {
+//
+//        }
     }
 
 

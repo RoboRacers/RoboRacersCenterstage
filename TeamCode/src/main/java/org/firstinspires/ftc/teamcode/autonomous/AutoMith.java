f1654d51fb1723d13a39e80e621067ba6cf3672a
Mithilessh2010 <Mithib2010@hotmail.com>
2023-09-30 11:18:25 -0700

    Adding states and Events and transition

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoMith.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoMith.java
@@ -1,28 +1,64 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 
 public class AutoMith {
-    boolean finished = false;
-    boolean propfound = false;
+
     private enum state{
-        DRIVE_TO_PROP,
-        LOCATE_PROP,
-        PLACE_PIXEL_ON_SPIKE,
-        LOCATE_BD,
-        PLACE_PIXEL_ON_BD,
-        LOCATE_STACK1,
-        LOCATE_STACK2,
-        LOCATE_STACK3,
-        GO_TO_STACK1,
-        GO_TO_STACK2,
-        GO_TO_STACK3,
-        PICKUP_PIXEL,
+        DRIVING_TOWARDS_A_SPIKE_MARKER,
+        ROBOT_REACHED_THE_SPIKE_MARKER,
+        DRIVING_TOWARDS_THE_BACK_BACKDROP,
+        REACHED_THE_BACKDROP,
+        DRIVING_TOWARDS_THE_STACK_ONE,
+        REACHED_STACK_ONE,
+        DRIVING_TOWARDS_STACK_TWO,
+        REACHED_STACK_TWO,
+        DRIVE_TOWARDS_BACKDROP,
+        LOOP,
     }
 
-     void findprop(){
-        if (propfound = false){
-            final state locateProp = state.LOCATE_PROP;
-            final state placepixelonspike = state.PLACE_PIXEL_ON_SPIKE;
+    private enum event{
+        DETECTED_THE_TEAM_PROP,
+        LOCATION_XYZ,
+        DROPPED_PURPLE_PIXEL,
+        LOCATION,
+        DROP_YELLOW_PIXEL,
+        LOCATION_STACK_ONE,
+        PIXEL_IS_IN_CLAW,
+        LOCATION_STACK_TWO,
+        TWO_PIXELS_IN_CLAW,
+        LOOP,
+    }
+    public void transition(event) {
+        switch (event) {
+            case event.DETECTED_THE_TEAM_PROP:
+                currentState = state.DRIVING_TOWARDS_A_SPIKE_MARKER;
+                break;
+            case event.LOCATION_XYZ:
+                currentState = state.ROBOT_REACHED_THE_SPIKE_MARKER;
+                break;
+            case event.DROPPED_PURPLE_PIXEL:
+                currentState = state.DRIVING_TOWARDS_THE_BACK_BACKDROP;
+                break;
+            case event.LOCATION:
+                currentState = state.REACHED_THE_BACKDROP;
+                break;
+            case event.DROP_YELLOW_PIXEL:
+                currentState = state.DRIVING_TOWARDS_THE_STACK_ONE;
+                break;
+            case event.LOCATION_STACK_ONE:
+                currentState = state.REACHED_STACK_ONE;
+                break;
+            case event.PIXEL_IS_IN_CLAW:
+                currentState = state.DRIVING_TOWARDS_STACK_TWO;
+                break;
+            case event.LOCATION_STACK_TWO:
+                currentState = state.REACHED_STACK_TWO;
+                break;
+            case event.TWO_PIXELS_IN_CLAW:
+                currentState = state.DRIVE_TOWARDS_BACKDROP;
+                break;
+            case event.LOOP:
+                currentState = state.LOOP;
+                break;
         }
-        if()
     }
 }

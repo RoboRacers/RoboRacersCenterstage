f4dfc8379e634cad066be4459324f568c927ca51
SwiftButShaky <alexmao2019@gmail.com>
2023-10-07 11:38:04 -0700

    skeletons for other SMs

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/launcherSM.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+public class launcherSM {
+
+    public enum STATE {
+        DRONE_LAUNCHER_LOADED,
+        DRONE_LAUNCHED,
+    }
+
+    public enum EVENT {
+        GAME_START,
+        DRONE_LAUNCH_BUTTON_PRESSED,
+    }
+
+    STATE currentState;
+
+    public launcherSM.STATE getState() {
+        return currentState;
+    }
+
+    public void transition(EVENT event) {
+        switch (event) {
+            case GAME_START:
+                currentState = launcherSM.STATE.DRONE_LAUNCHER_LOADED;
+                break;
+            case DRONE_LAUNCH_BUTTON_PRESSED:
+                currentState = launcherSM.STATE.DRONE_LAUNCHED;
+                break;
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/liftSM.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+public class liftSM {
+
+    public enum STATE {
+        LIFT_UP,
+        LIFT_DOWN,
+    }
+
+    public enum EVENT {
+
+    }
+}

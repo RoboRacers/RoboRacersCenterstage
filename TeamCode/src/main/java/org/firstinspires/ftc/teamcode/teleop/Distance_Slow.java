0896750f8a18e5f24181bc2e4a09b282301c9647
superiorcave <113873020+superiorcave@users.noreply.github.com>
2023-10-07 15:00:33 -0700

    Update Distance_Slow.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Distance_Slow.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Distance_Slow.java
@@ -19,6 +19,9 @@ public class Distance_Slow extends LinearOpMode {
     double driveSensitivity;
     double turnSensitivity;
     Pose2d roPos;
+    double speedchange;
+    double minspeed;
+    double maxspeed;
 
 
 
@@ -27,9 +30,11 @@ public class Distance_Slow extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
 
         robot = new RobotCore(hardwareMap, gamepad1, gamepad2);
-        driveSensitivity = 0.5;
-        turnSensitivity = 0.5;
-
+        speedchange = 0.05;
+        minspeed = 0.1;
+        maxspeed = 0.5;
+        driveSensitivity = maxspeed;
+        turnSensitivity = maxspeed;
 
 
         //runs once after init
@@ -47,6 +52,25 @@ public class Distance_Slow extends LinearOpMode {
             robot.drive.setWeightedDrivePower(new Pose2d(gamepad1.left_stick_y*driveSensitivity, -gamepad1.left_stick_x*driveSensitivity, -gamepad1.right_stick_x*turnSensitivity));
 
             roPos = robot.drive.getPoseEstimate(); //Get Position
+
+            //code gradually decreases the speed
+            if (roPos == roPos){ //this will change into if in range...somehow
+                if (driveSensitivity > minspeed){ // if speed is greater than the miniumum
+                    driveSensitivity = driveSensitivity-speedchange;//decrease the speed by the amount we want
+                    turnSensitivity = turnSensitivity-speedchange;
+                }else if(driveSensitivity <= minspeed){//if speed is less than or equal to miniumum
+                    driveSensitivity = minspeed; //makes speed miniumum
+                    turnSensitivity = minspeed;
+                }
+            }else{ //if outside of range...will change sometime
+                if (driveSensitivity < maxspeed){//if speed is less than max
+                    driveSensitivity = driveSensitivity + speedchange;//increase speed
+                    turnSensitivity = turnSensitivity + speedchange;
+                }else if(driveSensitivity >= maxspeed){//if sped is greaterthan or equal to max
+                    driveSensitivity = maxspeed;//make speed max
+                    turnSensitivity = maxspeed;
+                }
+            }
 
 
 

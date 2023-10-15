f4f5d754317bfc9682faf182576972a10b195125
SKS-Student <67933648+SKS-Student@users.noreply.github.com>
2023-10-15 11:17:18 -0700

    added servos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/subsystems/Intake.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.modules.subsystems;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.modules.statemachines.IntakeSM;
 import org.firstinspires.ftc.teamcode.modules.statemachines.StateMachine;
@@ -12,9 +13,20 @@ public class Intake extends Subsystem {
 
     public DcMotor intakeMotor;
 
+    public Servo claw;
+    public Servo claw_flip;
+    public Servo claw_extend_one;
+    public Servo claw_extend_two;
+
     double currentSpeed;
 
     public Intake(HardwareMap hardwareMap) {
+        //4 servos
+
+        claw = hardwareMap.get(Servo.class, "claw");
+        claw_flip = hardwareMap.get(Servo.class, "claw_flip");
+        claw_extend_one = hardwareMap.get(Servo.class, "claw_extend_one");
+        claw_extend_two = hardwareMap.get(Servo.class, "claw_extend_two");
 
         intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
 

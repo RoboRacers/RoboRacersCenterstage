72a741ab6f3bd2cf454f6eaf753632e331b8611c
superiorcave <113873020+superiorcave@users.noreply.github.com>
2023-10-15 12:07:37 -0700

    Update Slides.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/subsystems/Slides.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/subsystems/Slides.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.modules.subsystems;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.modules.statemachines.SlidesSM;
 
@@ -14,6 +15,20 @@ public class Slides extends Subsystem {
 
     double leftMotorPos;
     double rightMotorPos;
+
+    //PID STUFF
+    double integralSum = 0;
+    double kp = 0;
+    double ki = 0;
+    double kd = 0;
+    double error;
+    private double lasterror = 0;
+    double reference; //the point you want to go to
+    double state;
+
+    ElapsedTime timer = new ElapsedTime();
+
+
 
     public Slides(HardwareMap hardwareMap) {
 
@@ -28,10 +43,23 @@ public class Slides extends Subsystem {
         leftMotor.setTargetPosition(pos);
     }
 
+    public double PIDControl(double reference,double state) {
+        double error = reference - state;
+        integralSum += error * timer.seconds();
+        double derivative = (error- lasterror) / timer.seconds();
+        lasterror = error;
+
+        timer.reset();
+
+        double output = (error * kp) + (derivative * kd) + (integralSum * ki);
+        return output;
+    }
+
     @Override
     public void update() {
         leftMotorPos = leftMotor.getCurrentPosition();
         rightMotorPos = rightMotor.getCurrentPosition();
+        
 
         statemachine.update();
     }

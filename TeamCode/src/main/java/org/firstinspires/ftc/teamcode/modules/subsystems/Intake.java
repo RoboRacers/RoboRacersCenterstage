776b2382edb5d2f5e059550909d118df1100a158
SKS-Student <67933648+SKS-Student@users.noreply.github.com>
2023-10-15 12:08:34 -0700

    Set the motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/subsystems/Intake.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.modules.subsystems;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
@@ -16,6 +17,9 @@ public class Intake extends Subsystem {
     public Servo claw_extend_one;
     public Servo claw_extend_two;
 
+    public DcMotor leftMotor;
+    public DcMotor rightMotor;
+
     double currentSpeed;
 
     public Intake(HardwareMap hardwareMap) {
@@ -25,6 +29,8 @@ public class Intake extends Subsystem {
         claw_flip = hardwareMap.get(Servo.class, "claw_flip");
         claw_extend_one = hardwareMap.get(Servo.class, "claw_extend_one");
         claw_extend_two = hardwareMap.get(Servo.class, "claw_extend_two");
+        leftMotor = hardwareMap.get(DcMotor.class, "leftMotor");
+        rightMotor = hardwareMap.get(DcMotor.class, "rightMotor");
 
         statemachine = new IntakeSM(this);
     }
@@ -34,6 +40,9 @@ public class Intake extends Subsystem {
     public void setClawExtendOnePos(int pos3) {claw_extend_one.setPosition(pos3);}
     public void setClawExtendTwoPos(int pos4) {claw_extend_two.setPosition(pos4);}
     public void setClawExtendTwoDirection(Servo.Direction direction){claw_extend_two.setDirection(direction);}
+    public void setLeftMotorSpeed(double speed1) {leftMotor.setPower(speed1);}
+    public void setRightMotorSpeed(double speed2) {rightMotor.setPower(speed2);}
+    public void setLeftMotorDirection(DcMotorSimple.Direction direction) {leftMotor.setDirection(direction);}
 
     @Override
     public void update() {

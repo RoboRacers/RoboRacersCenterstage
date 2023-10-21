8fc4c5b8aaffc0f6429c6cac5f25cc577cee00d6
Vikram Kommera <outcastvirus24@icloud.com>
2023-10-21 12:33:50 -0700

    Replace with RR 1.0

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/statemachines/StateMachine.java
+++
@@ -1,6 +0,0 @@
-package org.firstinspires.ftc.teamcode.modules.statemachines;
-
-public interface StateMachine {
-
-    public void update();
-}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/subsystems/Subsystem.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode.modules.subsystems;
-
-import org.firstinspires.ftc.teamcode.modules.statemachines.StateMachine;
-
-public abstract class Subsystem {
-
-    public abstract void update();
-
-}

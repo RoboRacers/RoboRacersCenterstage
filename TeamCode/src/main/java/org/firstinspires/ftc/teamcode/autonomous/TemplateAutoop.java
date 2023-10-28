884769099cf5859f87c044e74dfaf6822c741a4e
Vikram Kommera <outcastvirus24@icloud.com>
2023-10-28 12:12:51 -0700

    Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/TemplateAutoop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/TemplateAutoop.java
@@ -1,11 +1,22 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 
+import androidx.annotation.NonNull;
+
 import com.acmerobotics.dashboard.config.Config;
-import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.ParallelAction;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.RobotCore;
+import org.firstinspires.ftc.teamcode.modules.drive.MecanumDrive;
+import org.firstinspires.ftc.teamcode.modules.drive.MecanumDrive.FollowTrajectoryAction;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
 // Localization is doesn't show drift, follower if it does
 
@@ -13,13 +24,18 @@ import org.firstinspires.ftc.teamcode.RobotCore;
 @Autonomous(name = "AutoOp State Machines", group = "16481-Power-Play")
 public class TemplateAutoop extends LinearOpMode {
 
-    boolean finished = false;
+    public static boolean finished = false;
+
+    static ElapsedTime time;
 
     @Override
     public void runOpMode() {
 
         RobotCore robot = new RobotCore(hardwareMap);
 
+        FollowTrajectoryAction traj1 = (FollowTrajectoryAction) robot.drive.actionBuilder(robot.drive.pose)
+                .splineTo(new Vector2d(15, 15), Math.toRadians(90))
+                .build();
 
 
         while(!isStopRequested() && !opModeIsActive()) {
@@ -30,11 +46,9 @@ public class TemplateAutoop extends LinearOpMode {
 
         if (isStopRequested()) return;
 
-        while(opModeIsActive() && !finished){
-
-            
-            finished = true;
-        }
+        Actions.runBlocking(
+                traj1
+        );
 
     }
 

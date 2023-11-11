c8c8814c6dd8a216caf2926db93847c0f4f2c3ea
SKS-Student <67933648+SKS-Student@users.noreply.github.com>
2023-11-11 13:17:10 -0800

    Testing the slides code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/SLIDES.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/SLIDES.java
@@ -2,32 +2,22 @@ package org.firstinspires.ftc.teamcode.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp(name = "Launcher_TEST", group = "16481-Power-Play")
+@TeleOp(name = "Slides_TEST", group = "16481-Power-Play")
 public class SLIDES extends LinearOpMode {
 
 //Servo drone;
-//DcMotorEx slides1;
-//DcMotorEx slides2;
+DcMotorEx slides1;
+DcMotorEx slides2;
 
-Servo Bs1;
-Servo Bs2;
-Servo Ss1;
-Servo Ss2;
-Servo Cs1;
 ElapsedTime timer = new ElapsedTime();
 
-double Increment = 0.001;
-double MINPOS = 0.0;
-double MAXPOS = 1.0;
-double position1 = 0.5;
-double position2 = 0.6;
-double position3 = 0.5;
-double position4 = 0.5;
-double position5 = 0.0;
-
+double slidesPower1 = 0.0;
+double slidesPower2 = 0.0;
 
 
    @Override
@@ -35,76 +25,38 @@ double position5 = 0.0;
 
 
        // drone = hardwareMap.get(Servo.class, "drone");
-     //   slides1 = hardwareMap.get(DcMotorEx.class, "slides1");
-      //  slides2 = hardwareMap.get(DcMotorEx.class, "slides2");
-      //  slides2.setDirection(DcMotorSimple.Direction.REVERSE);
-        Bs1 = hardwareMap.get(Servo.class, "Bs1");
-        Bs2 = hardwareMap.get(Servo.class, "Bs2");
-        Ss1 = hardwareMap.get(Servo.class, "Ss1");
-        Ss2 = hardwareMap.get(Servo.class, "Ss2");
-        Cs1 = hardwareMap.get(Servo.class, "Cs1");
+     //
+        slides1 = hardwareMap.get(DcMotorEx.class, "slides1");
+        slides2 = hardwareMap.get(DcMotorEx.class, "slides2");
 
         while (opModeInInit()){
-            Bs2.setDirection(Servo.Direction.REVERSE);
-            Ss2.setDirection(Servo.Direction.REVERSE);
-            Cs1.setDirection(Servo.Direction.REVERSE);
+           slides2.setDirection(DcMotorSimple.Direction.REVERSE);
             }
         while (opModeIsActive()){
-            if(gamepad1.dpad_up){
-                position1 += Increment;
-                position2 += Increment;
-                if (position2 >= MAXPOS){
-                    position2 = MAXPOS;
-                    position1 = MAXPOS - 0.1;
-                }
-            }
-            else if(gamepad1.dpad_down){
-                position1 -= Increment;
-                position2 -= Increment;
-                if (position1 <= MINPOS){
-                    position1 = MINPOS;
-                    position2 = MINPOS + 0.1;
-                }
-            }
-            if(gamepad1.dpad_left){
-                position3 += Increment;
-                position4 += Increment;
-                if (position3 >= MAXPOS){
-                    position3 = MAXPOS;
-                    position4 = MAXPOS;
-                }
-            }
-            else if(gamepad1.dpad_right){
-                position3 -= Increment;
-                position4 -= Increment;
-                if (position4 <= MINPOS){
-                    position3 = MINPOS;
-                    position4 = MINPOS;
-                }
-            }
-            if (gamepad1.left_bumper){
-                position5 = 0.54;
-            }else if(gamepad1.right_bumper){
-                position5 = 0.8;
-            }
-            
-            set1(position1, position2, position3, position4, position5);
-            telemetry.addData("Bs1 Value", Bs1.getPosition());
-            telemetry.addData("Bs2 Value", Bs2.getPosition());
-            telemetry.addData("Ss1 Value", Ss1.getPosition());
-            telemetry.addData("Ss1 Value", Ss2.getPosition());
-            telemetry.addData("Cs1 Value", Cs1.getPosition());
-            telemetry.update();
+
+          if (gamepad1.right_trigger > 0){
+              slidesPower1 = 0.2;
+              slidesPower2 = 0.2;
+          }
+
+          else if (gamepad1.left_trigger > 0){
+                slidesPower1 = -0.2;
+                slidesPower2 = -0.2;
+          }
+          else {
+                slidesPower1 = 0.0;
+                slidesPower2 = 0.0;
+          }
+
+            set2(slidesPower1, slidesPower2);
+           telemetry.update();
 
         }
 telemetry.update();
     }
-    public void set1(double pos1, double pos2, double pos3, double pos4, double pos5){
-        Bs1.setPosition(pos1);
-        Bs2.setPosition(pos2);
-        Ss1.setPosition(pos3);
-        Ss2.setPosition(pos4);
-        Cs1.setPosition(pos5);
+    public void set2(double power1, double power2){
+       slides1.setPower(power1);
+       slides2.setPower(power2);
     }
 
 }

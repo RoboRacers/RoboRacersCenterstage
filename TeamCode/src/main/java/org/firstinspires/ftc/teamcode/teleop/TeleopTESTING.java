ce79af3a3fcdfe5ba1351c776d0ff3f1678d877b
SKS-Student <67933648+SKS-Student@users.noreply.github.com>
2023-11-11 10:39:41 -0800

    Testing the slides code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleopTESTING.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleopTESTING.java
@@ -16,103 +16,47 @@ import org.firstinspires.ftc.teamcode.modules.subsystems.Launcher;
 @TeleOp(name = "Launcher_TEST", group = "16481-Power-Play")
 public class TeleopTESTING extends LinearOpMode {
 
-    double averagePos;
-
-    //PID STUFF
-    double integralSum = 0;
-    double kp = 0;
-    double ki = 0;
-    double kd = 0;
-    private double lasterror = 0;
-
 //Servo drone;
-DcMotorEx slides1;
-DcMotorEx slides2;
+//DcMotorEx slides1;
+//DcMotorEx slides2;
 
-Servo s1;
-Servo s2;
-Servo c1;
+Servo Fs1;
+Servo Fs2;
+Servo Ss1;
+Servo Ss2;
 ElapsedTime timer = new ElapsedTime();
+
+double MINPOS = 0.0;
+double MAXPOS = 1.0;
+
 
    @Override
     public void runOpMode(){
 
 
        // drone = hardwareMap.get(Servo.class, "drone");
-        slides1 = hardwareMap.get(DcMotorEx.class, "slides1");
-        slides2 = hardwareMap.get(DcMotorEx.class, "slides2");
-        slides2.setDirection(DcMotorSimple.Direction.REVERSE);
-        s1 = hardwareMap.get(Servo.class, "s1");
-        s2 = hardwareMap.get(Servo.class, "s2");
-        s2.setDirection(Servo.Direction.REVERSE);
-        c1 = hardwareMap.get(Servo.class, "c1");
+     //   slides1 = hardwareMap.get(DcMotorEx.class, "slides1");
+      //  slides2 = hardwareMap.get(DcMotorEx.class, "slides2");
+      //  slides2.setDirection(DcMotorSimple.Direction.REVERSE);
+        Fs1 = hardwareMap.get(Servo.class, "fs1");
+        Fs2 = hardwareMap.get(Servo.class, "fs2");
+        Ss1 = hardwareMap.get(Servo.class, "Ss1");
+Ss2 = hardwareMap.get(Servo.class, "Ss2");
 
         while (opModeInInit()){
-            set1(0.25);
+            Fs2.setDirection(Servo.Direction.REVERSE);
+            Ss2.setDirection(Servo.Direction.REVERSE);
             }
         while (opModeIsActive()){
-            set1(0);
-//            if (gamepad1.dpad_up) {
-       //     slidesSet(0.2);
-//            }
-//            if (gamepad1.dpad_down) {
-//                slidesSet(-0.2);
-//            }
-//            else {
-//                slidesSet(0.0);
-//            }
-            /*
-            if (gamepad1.a){
-                drone.setPosition(0.25);
-                telemetry.addData("Drone is being launched", "");
-            }
-            else if (gamepad1.b){
-                drone.setPosition(0.00);
-                telemetry.addData("Drone is not being launched", "");
-            }
-             */
+
+                Fs1.setPosition(0.25);
 
         }
 telemetry.update();
     }
     public void set1(double pos1){
-        s1.setPosition(pos1);
-        s2.setPosition(pos1);
-    }
-    public void clawopen(boolean set){
-        double close = 0.0;
-        double open = 0.25;
-        if (set){
-            c1.setPosition(open);
-        }else{
-            c1.setPosition(close);
-        }
-
+        Fs1.setPosition(pos1);
+        Fs2.setPosition(pos1);
     }
 
-
-
-
-
-public void slidesSet(double slidesPower){
-       slides1.setPower(slidesPower);
-       slides2.setPower(slidesPower);
 }
-
-
-    /*
-    public void setLiftPosition(int pos) {
-        slides.setTargetPosition(pos);
-    }
-    public double PIDcontrol(double reference, double state){
-       double error = reference - state;
-       integralSum += error * timer.seconds();
-       double derivative = (error-lasterror) / timer.seconds();
-       lasterror = error;
-       timer.reset();
-       double output = (error*kp) + (derivative *kd) + (integralSum*ki);
-               return output;
-    }
-    */
-}
-            // Telemetry
